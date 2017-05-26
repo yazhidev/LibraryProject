b6d533346b69cf08e426ecb62d10e11827050a8a
曾亚智 <zengyazhi@wisezone.org>
2017-05-26 13:56:10 +0800

    add some mathod of Utils

--- yazhilib/src/main/java/com/yazhi1992/yazhilib/utils/CalcUtil.java
+++ yazhilib/src/main/java/com/yazhi1992/yazhilib/utils/CalcUtil.java
@@ -3,6 +3,11 @@ package com.yazhi1992.yazhilib.utils;
 import android.content.Context;
 import android.util.TypedValue;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
 /**
  * Created by zengyazhi on 17/5/17.
  */
@@ -17,6 +22,36 @@ public class CalcUtil {
     public static float dx2dp(Context Context, int px) {
         final float scale = Context.getResources().getDisplayMetrics().densityDpi;
         return (px * 160) / scale + 0.5f;
+    }
+
+    /**
+     * 得到文件的MD5值
+     *
+     * @param path 文件地址
+     * @return
+     */
+    public static String getFileMD5(String path) {
+        File file = new File(path);
+        if (!file.isFile()) {
+            return null;
+        }
+        MessageDigest digest = null;
+        FileInputStream in = null;
+        byte buffer[] = new byte[1024];
+        int len;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BigInteger bigInt = new BigInteger(1, digest.digest());
+        return bigInt.toString(16);
     }
 
 }
---
+++ yazhilib/src/main/java/com/yazhi1992/yazhilib/utils/TimeUtils.java
@@ -0,0 +1,53 @@
+package com.yazhi1992.yazhilib.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by zengyazhi on 17/5/24.
+ */
+
+public class TimeUtils {
+
+    private static String[] dayInWeekTime = {"", "周一", "周二", "周三", "周四", "周五", "周六", "周日"};
+
+    /**
+     * 计算周几
+     *
+     * @param date
+     * @return
+     */
+    public static String getDayInWeek(Date date) {
+        Calendar instance = Calendar.getInstance();
+        instance.setTime(date);
+        int dayOfWeek = instance.get(Calendar.DAY_OF_WEEK);
+        if (instance.getFirstDayOfWeek() == Calendar.SUNDAY) {
+            //如果一周第一天为星期天，则返回 1日，2一，3二。。以此类推
+            //如果一周第一天为星期一，则返回 1一，2二，3三。。以此类推
+            dayOfWeek -= 1;
+            if (dayOfWeek == 0) {
+                //周日
+                dayOfWeek = 7;
+            }
+        }
+        return dayInWeekTime[dayOfWeek];
+    }
+
+    /**
+     * 计算周几
+     *
+     * @param format 时间格式
+     * @param time   时间字符串
+     * @return
+     */
+    public static String getDayInWeek(SimpleDateFormat format, String time) {
+        try {
+            return getDayInWeek(format.parse(time));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
