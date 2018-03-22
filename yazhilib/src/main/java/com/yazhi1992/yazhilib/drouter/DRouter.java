b94a487b34b6c98703a36808aff82be2cc58cc46
zengyazhi <yazhi1992@163.com>
2018-03-22 13:06:20 +0800

    calendar

---
+++ yazhilib/src/main/java/com/yazhi1992/yazhilib/drouter/DRouter.java
@@ -0,0 +1,8 @@
+package com.yazhi1992.yazhilib.drouter;
+
+/**
+ * Created by zengyazhi on 2018/3/9.
+ */
+
+public class DRouter {
+}
---
+++ yazhilib/src/main/java/com/yazhi1992/yazhilib/drouter/annnotation/ViewInjectUtils.java
@@ -0,0 +1,13 @@
+package com.yazhi1992.yazhilib.drouter.annnotation;
+
+import android.app.Activity;
+
+/**
+ * Created by zengyazhi on 2018/3/9.
+ */
+
+public class ViewInjectUtils {
+    public static void inject(Activity activity) {
+
+    }
+}
